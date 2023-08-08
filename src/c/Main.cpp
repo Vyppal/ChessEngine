818d5aface90a6a0084596fd3b52cf5e49de9e54
Isaac Turner <spacey_sooty@outlook.com>
2023-08-08 20:51:08 +0800

    fixed stupid gitignore

---
+++ src/c/Main.cpp
@@ -0,0 +1,5 @@
+#include "Main.hpp"
+
+int main() {
+  return 0;
+};
---
+++ src/include/GameHandler.hpp
@@ -0,0 +1,84 @@
+#include <vector>
+
+double timeControl = 120;
+
+struct GameState {
+  std::vector<char> boardLayout;
+  double whiteTime;
+  double blackTime;
+  bool whiteCastleRights;
+  bool blackCastleRights;
+};
+
+
+enum Pieces {
+  white_pawn,
+  white_knight,
+  white_bishop,
+  white_rook,
+  white_queen,
+  white_king,
+  black_pawn,
+  black_knight,
+  black_bishop,
+  black_rook,
+  black_queen,
+  black_king,
+  empty
+};
+
+
+
+
+
+
+
+class GameHandler {
+
+ public:
+
+  GameState GetGameState(){
+    return GameState{
+      board,
+      whiteTime,
+      blackTime,
+      whiteCastleRights,
+      blackCastleRights
+    };
+};
+
+  void RecieveMove() {}
+
+  bool ValidateMove() {}
+
+  // lichess api
+
+ private:
+  // std::vector<char> board = {
+  // /* a    b    c    d    e    f    g    h*/
+  //   'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r', /* 8 */
+  //   'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', /* 7 */
+  //   ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', /* 6 */
+  //   ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', /* 5 */
+  //   ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', /* 4 */
+  //   ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', /* 3 */
+  //   'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', /* 2 */
+  //   'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'  /* 1 */
+  // };
+
+  std::vector<char> board = {
+    Pieces::black_rook, Pieces::black_knight, Pieces::black_bishop, Pieces::black_queen, Pieces::black_king, Pieces::black_bishop, Pieces::black_knight, Pieces::black_rook,
+    Pieces::black_pawn, Pieces::black_pawn, Pieces::black_pawn, Pieces::black_pawn, Pieces::black_pawn, Pieces::black_pawn, Pieces::black_pawn, Pieces::black_pawn,
+    Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty,
+    Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty,
+    Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty,
+    Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty,
+    Pieces::white_pawn, Pieces::white_pawn, Pieces::white_pawn, Pieces::white_pawn, Pieces::white_pawn, Pieces::white_pawn, Pieces::white_pawn, Pieces::white_pawn, 
+    Pieces::white_rook, Pieces::white_knight, Pieces::white_bishop, Pieces::white_queen, Pieces::white_king, Pieces::white_bishop, Pieces::white_knight, Pieces::white_rook
+  };
+
+  double whiteTime = timeControl;
+  double blackTime = timeControl;
+  bool whiteCastleRights = true;
+  bool blackCastleRights = true;
+};
