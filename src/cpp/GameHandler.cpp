242c0ddf8b36f4896a99252504a0728db7b46f8b
Vyppal <86544866+Vyppal@users.noreply.github.com>
2023-08-12 11:55:45 +0800

    new branch

---
+++ src/cpp/GameHandler.cpp
@@ -0,0 +1,17 @@
+#include "GameHandler.hpp"
+
+GameHandler::GameHandler() {}
+
+void GameHandler::GiveMove(int tileFrom[2], int tileTo[2], char promotionPiece=' ') {
+
+}
+
+bool GameHandler::ValidateMove() {
+ /*
+ 
+  for validating a move
+    check if requested move colour is correct colour
+    
+ 
+ */
+}
--- src/include/GameHandler.hpp
+++ src/include/GameHandler.hpp
@@ -1,4 +1,7 @@
+#pragma once
+
 #include <vector>
+#include "Piece.hpp"
 
 double timeControl = 120;
 
@@ -14,20 +17,11 @@ struct GameState {
   bool blackLongCastleRights;
 };
 
-enum Pieces {
-  white_pawn,
-  white_knight,
-  white_bishop,
-  white_rook,
-  white_queen,
-  white_king,
-  black_pawn,
-  black_knight,
-  black_bishop,
-  black_rook,
-  black_queen,
-  black_king,
-  empty
+/* PieceColour, b=black, w=white, empty */
+enum PC {
+  b,
+  w,
+  e
 };
 
 // surely each piece should be a class which derives from a "piece class which has things like state"
@@ -35,47 +29,25 @@ enum Pieces {
 class GameHandler {
  public:
 
-  GameState GetGameState(){
-    return GameState{
-      board,
-      whiteTime,
-      blackTime,
-      whiteShortCastleRights,
-      blackShortCastleRights,
-      whiteLongCastleRights,
-      blackLongCastleRights
-    };
-};
+  GameHandler();
 
-  void RecieveMove() {} // surely this should return a move to be validated and used to change the board
+  GameState GetGameState();
 
-  bool ValidateMove() {} // surely this has to take in a move 
+  /**
+   * @brief Use this to send the gamehandler your choice of move
+   * @param tileFrom An integer array of size 2 (fileNumber, rankNumber) representing the tile from which your moved piece is
+   * @param tileTo An integer array of size 2 (fileNumber, rankNumber) representing the tile which you want your piece to move to
+   * @param promotionPiece The capitalised algebraic notation of a piece a pawn should promote to. By default it's none.
+  */
+  void GiveMove(int tileFrom[2], int tileTo[2], char promotionPiece=' ');
+
+  bool ValidateMove(); // surely this has to take in a move 
 
   // lichess api ??
 
  private:
-  // std::vector<char> board = {
-  // /* a    b    c    d    e    f    g    h*/
-  //   'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r', /* 8 */
-  //   'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', /* 7 */
-  //   ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', /* 6 */
-  //   ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', /* 5 */
-  //   ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', /* 4 */
-  //   ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', /* 3 */
-  //   'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', /* 2 */
-  //   'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'  /* 1 */
-  // }; dunno what this was meant to be
-
-  std::vector<char> board = {
-    Pieces::black_rook, Pieces::black_knight, Pieces::black_bishop, Pieces::black_queen, Pieces::black_king, Pieces::black_bishop, Pieces::black_knight, Pieces::black_rook,
-    Pieces::black_pawn, Pieces::black_pawn, Pieces::black_pawn, Pieces::black_pawn, Pieces::black_pawn, Pieces::black_pawn, Pieces::black_pawn, Pieces::black_pawn,
-    Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty,
-    Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty,
-    Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty,
-    Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty, Pieces::empty,
-    Pieces::white_pawn, Pieces::white_pawn, Pieces::white_pawn, Pieces::white_pawn, Pieces::white_pawn, Pieces::white_pawn, Pieces::white_pawn, Pieces::white_pawn, 
-    Pieces::white_rook, Pieces::white_knight, Pieces::white_bishop, Pieces::white_queen, Pieces::white_king, Pieces::white_bishop, Pieces::white_knight, Pieces::white_rook
-  }; // nice
+  bool isWhitesTurn = true;
+  std::vector<Piece> _pieces;
 
   double whiteTime = timeControl;
   double blackTime = timeControl;
